0258a2e58fe8f72a8f45eecff9704ca33705c12a
yenvy07100105@gmail.com <yenvy07100105@gmail.com>
2026-06-05 18:49:28 +0700

    Auto commit local changes (20260605-184913)

--- DOI SOAT CHI PHI/NAM 2025/THANG 12/BANK/convert_to_xls.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Gọi PowerShell script để convert Excel từ cùng thư mục với file .bat
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0do_not_touch_convert_excel.ps1"
-pause
--- KIỂM TOÁN NGÂN HÀNG/2025/THÁNG 12/TONG_HOP_KET_QUA_KINH_DOANH_THANG_12.2025/Tháng 12-CHI PHI/run-merge-excel.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Gọi PowerShell script để tổng hợp dữ liệu SHOPEE
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0merge-excel.ps1"
-pause
